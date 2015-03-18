c1a98306cc74d4356d552c40bdd4b7c634fb20be
Santi Herrero <santihbc@gmail.com>
2015-03-18 15:51:16 +0100

    add default value for the time observer interval

--- Classes/ios/VKVideoPlayer.m
+++ Classes/ios/VKVideoPlayer.m
@@ -526,6 +526,10 @@ typedef enum {
   if (avPlayer) {
     __weak __typeof(self) weakSelf = self;
     [avPlayer addObserver:self forKeyPath:@"status" options:0 context:nil];
+    
+    if (!self.timeObserverInterval) {
+        self.timeObserverInterval = CMTimeMake(1, 1);
+    }
     self.timeObserver = [avPlayer addPeriodicTimeObserverForInterval:self.timeObserverInterval queue:NULL usingBlock:^(CMTime time){
       [weakSelf periodicTimeObserver:time];
     }];
